223aebe6ec6cda51ad4eb976d248f768fbb16ed9
LucasLealAI <lucas2006leal@gmail.com>
2025-10-28 16:21:54 -0300

    Consertado login

--- menu.py
+++ menu.py
@@ -1,7 +1,10 @@
 print('Seja bem-vindo ao FEIFOOD!')
 escolha = input('Se for um novo usuário, digite C para se cadastrar. Caso já tenha logado, digite L para se logar: ')
 
-usuarios = {}
+usuarios = {
+      "Tania", 123,
+      "Lucas", 321
+}
 
 def menu():
       print("Selecione a comida que deseja:")
@@ -14,17 +17,25 @@ def menu():
 
 if escolha == "C":
         nome = input('Digite seu nome de usuário: ')
+        senha = input('Digite uma senha: ')
         if nome in usuarios:
-            print("Usuário já cadastrado, tente novamente")
+              print("Usuário já presente, por favor, faça o login ao invés.")
+        elif senha in usuarios:
+              print("Usuário já presente, por favor, faça o login ao invés.")
         else:
-            senha = input('Digite uma senha: ')
-            usuarios[nome] = senha
-            print("Usuário cadastro com sucesso!")
+            print("Cadastro feito com sucesso, aproveite sua primeira vez!")
             menu()
+              
 elif escolha == "L":
         nome = input('Digite seu nome de usuário: ')
         senha = input('Digite uma senha: ')
-        if nome in usuarios and usuarios[nome] == senha:
-            print("Login cadastrado com sucesso!")
+        if nome in usuarios:
+            print("Login completo, aproveite!")
             menu()
-
+        elif senha in usuarios:
+              print("Login completo, aproveite!")
+              menu()
+        else:
+              print("Usuário não encontrado, caso não há uma conta, cadastre-se!")
+else:
+      print("Opção inválida.")
