ea2c706b2b37e508e46fc83ad18bed6cb8236d05
LucasLealAI <lucas2006leal@gmail.com>
2025-10-28 15:43:13 -0300

    Its peak...

--- menu.py
+++ menu.py
@@ -3,6 +3,15 @@ escolha = input('Se for um novo usuário, digite C para se cadastrar. Caso já t
 
 usuarios = {}
 
+def menu():
+      print("Selecione a comida que deseja:")
+      comidas = {
+            "Hamburguer": "R$30,00",
+            "Hot Dog": "R$25,00",
+            "Coxinha": "R$20,00"
+      }
+      print(comidas)
+
 if escolha == "C":
         nome = input('Digite seu nome de usuário: ')
         if nome in usuarios:
@@ -11,8 +20,11 @@ if escolha == "C":
             senha = input('Digite uma senha: ')
             usuarios[nome] = senha
             print("Usuário cadastro com sucesso!")
+            menu()
 elif escolha == "L":
         nome = input('Digite seu nome de usuário: ')
         senha = input('Digite uma senha: ')
         if nome in usuarios and usuarios[nome] == senha:
             print("Login cadastrado com sucesso!")
+            menu()
+
